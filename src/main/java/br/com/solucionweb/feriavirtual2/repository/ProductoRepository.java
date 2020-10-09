9b347c0d8f9981147fe063d948812ce3f2f11af5
Marcelo Rodrigues <marcelo.rodrigues@metasix.com.br>
2020-10-09 12:55:54 -0300

    teste login

--- src/main/java/br/com/solucionweb/feriavirtual2/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.solucionweb.feriavirtual2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.solucionweb.feriavirtual2.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long>{
-
-}
