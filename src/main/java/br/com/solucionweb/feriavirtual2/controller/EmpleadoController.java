6c567dc69db35b1da5cf396528f01a800c8fe958
Marcelo-fan-Elixir <mr.leite73@gmail.com>
2020-10-22 10:30:04 -0300

    CRUD EMPLEADO FINALIZADO

--- src/main/java/br/com/solucionweb/feriavirtual2/controller/EmpleadoController.java
+++ src/main/java/br/com/solucionweb/feriavirtual2/controller/EmpleadoController.java
@@ -1,12 +1,16 @@
 package br.com.solucionweb.feriavirtual2.controller;
-
+ 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +40,33 @@ public class EmpleadoController {
 	@GetMapping()
 	public ResponseEntity<List<EmpleadoDto>> listEmpleado(){
 		return ResponseEntity.ok(new EmpleadoDto().convertToList(empleadoService.listEmpleado()));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<EmpleadoDto> getEmpleado(@PathVariable Long id){
+		Optional<Empleado> emplado = empleadoService.getEmpleado(id);
+		if (emplado.isPresent()) {
+			return ResponseEntity.noContent().build();//204
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteEmpleado(@PathVariable Long id){
+		Optional<Empleado> empleado = empleadoService.deleteEmpleado(id);
+		if (empleado.isPresent()) {
+			return ResponseEntity.noContent().build();//204
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateEmpleado(@PathVariable Long id,@RequestBody EmpleadoForm empleadoForm){
+		Optional<Empleado> empleado = empleadoService.updateEmpleado(id, empleadoForm);
+		if(empleado.isPresent()) {
+			return ResponseEntity.ok().body(new EmpleadoDto(empleado.get()));	
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 }
--- src/main/java/br/com/solucionweb/feriavirtual2/service/impl/EmpleadoServiceImpl.java
+++ src/main/java/br/com/solucionweb/feriavirtual2/service/impl/EmpleadoServiceImpl.java
@@ -27,22 +27,62 @@ public class EmpleadoServiceImpl implements EmpleadoService{
 
 	@Override
 	public Optional<Empleado> getEmpleado(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return empleadoRepository.findById(id);
 	}
 
 	@Override
 	@Transactional
 	public Optional<Empleado> deleteEmpleado(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Empleado> empleado = empleadoRepository.findById(id);
+		if (empleado.isPresent()) {
+			empleadoRepository.deleteById(id);
+			return empleado;
+		}
+		return empleado;
 	}
 
 	@Override
 	@Transactional
 	public Optional<Empleado> updateEmpleado(Long id, EmpleadoForm empleadoForm) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Empleado> empleado = empleadoRepository.findById(id);
+		if(empleado.isPresent()) {
+			Empleado empleadoUpdated = empleadoRepository.getOne(id);
+			empleadoUpdated.setRutEmpleado(empleadoForm.getRutEmpleado() != null? empleadoForm.getRutEmpleado() :
+				(empleadoUpdated.getRutEmpleado() != null? empleadoUpdated.getRutEmpleado() : null));
+			
+			empleadoUpdated.setDvEmpleado(empleadoForm.getDvEmpleado() != null? empleadoForm.getDvEmpleado() :
+				(empleadoUpdated.getDvEmpleado() != null? empleadoUpdated.getDvEmpleado() : null));
+			
+			empleadoUpdated.setNombreEmpleado(empleadoForm.getNombreEmpleado() != null? empleadoForm.getNombreEmpleado() :
+				(empleadoUpdated.getNombreEmpleado() != null? empleadoUpdated.getNombreEmpleado() : null));
+			
+			empleadoUpdated.setApellidoEmpleado(empleadoForm.getApellidoEmpleado() != null? empleadoForm.getApellidoEmpleado() :
+				(empleadoUpdated.getApellidoEmpleado() != null? empleadoUpdated.getApellidoEmpleado() : null));
+			
+			empleadoUpdated.setFechaEmpleado(empleadoForm.getFechaEmpleado() != null? empleadoForm.getFechaEmpleado() :
+				(empleadoUpdated.getFechaEmpleado() != null? empleadoUpdated.getFechaEmpleado() : null));
+			
+			empleadoUpdated.setDireccionEmpleado(empleadoForm.getDireccionEmpleado() != null? empleadoForm.getDireccionEmpleado() :
+				(empleadoUpdated.getDireccionEmpleado() != null? empleadoUpdated.getDireccionEmpleado() : null));
+			
+			empleadoUpdated.setTelefonoEmpleado(empleadoForm.getTelefonoEmpleado() != null? empleadoForm.getTelefonoEmpleado() :
+				(empleadoUpdated.getTelefonoEmpleado() != null? empleadoUpdated.getTelefonoEmpleado() : null));
+			
+			empleadoUpdated.setEmailEmpleado(empleadoForm.getEmailEmpleado() != null? empleadoForm.getEmailEmpleado() :
+				(empleadoUpdated.getEmailEmpleado() != null? empleadoUpdated.getEmailEmpleado() : null));
+			
+			empleadoUpdated.setTipoEmpleado(empleadoForm.getTipoEmpleado() != null? empleadoForm.getTipoEmpleado() :
+				(empleadoUpdated.getTipoEmpleado() != null? empleadoUpdated.getTipoEmpleado() : null));
+			
+			empleadoUpdated.setSexo(empleadoForm.getSexo() != null? empleadoForm.getSexo() :
+				(empleadoUpdated.getSexo() != null? empleadoUpdated.getSexo() : null));
+			
+			empleadoUpdated.setComuna(empleadoForm.getComuna() != null? empleadoForm.getComuna() :
+				(empleadoUpdated.getComuna() != null? empleadoUpdated.getComuna() : null));
+
+			return empleado;
+		}
+		return empleado;
 	}
 
 	@Override
